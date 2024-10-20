aa8067358a2672f3026af957ca710e672b796c40
Adarsh Hegde <141290903+AdarsHH30@users.noreply.github.com>
2024-10-20 12:49:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+genai.configure(api_key="AIzaSyB9GZWRqOO9SL7L5Lm-Sd39MscOtK8gNzQ")
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+chat = model.start_chat(
+  history=[
+    {"role": "model", "parts": ["Hello! How can I assist you today?"]},
+  ]
+)
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    data = request.get_json()
+    user_message = data.get('message')
+    
+    if user_message:
+        response = chat.send_message(user_message)
+        return jsonify({'reply': response.text})
+    else:
+        return jsonify({'error': 'No message provided'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
